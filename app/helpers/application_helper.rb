c1260e635f36a9b120c36d6a6f3412269186b7eb
charlie-elverson <charlie.elverson@gmail.com>
2016-10-05 20:11:32 -0600

    setup title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+#Returns full title for each page
+	def full_title(page='')
+		base = "Ruby on Rails Tutorial Sample App"
+		if page.empty?
+			base
+		else
+			page + " | " + base
+		end
+	end
+
 end
